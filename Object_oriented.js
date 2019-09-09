e2c76f1209bd8f0318561e8140827ddf1926885b
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-09-08 22:01:13 -0700

    +1 Object Oriented challenge

--- Object_oriented.js
+++ Object_oriented.js
@@ -84,7 +84,7 @@ dog.sayLegs();
 // sayName: function() {return "The name of this duck is " + duck.name + ".";}
 //
 // While this is a valid way to access the object's property, there is a pitfall here. If the variable name changes, any code referencing the original name would need to be updated as well. In a short object definition, it isn't a problem, but if an object has many references to its properties there is a greater chance for error.
-// 
+//
 // A way to avoid these issues is with the this keyword:
 //
 // let duck = {
@@ -107,3 +107,31 @@ let dog = {
 };
 
 dog.sayLegs();
+
+//
+//
+// Object Oriented Programming: Define a Constructor Function
+// Constructors are functions that create new objects. They define properties and behaviors that will belong to the new object. Think of them as a blueprint for the creation of new objects.
+//
+// Here is an example of a constructor:
+//
+// function Bird() {
+//   this.name = "Albert";
+//   this.color = "blue";
+//   this.numLegs = 2;
+// }
+// This constructor defines a Bird object with properties name, color, and numLegs set to Albert, blue, and 2, respectively.
+//
+// Constructors follow a few conventions:
+//
+// Constructors are defined with a capitalized name to distinguish them from other functions that are not constructors.
+// Constructors use the keyword this to set properties of the object they will create. Inside the constructor, this refers to the new object it will create.
+// Constructors define properties and behaviors instead of returning a value as other functions might.
+//
+//
+// Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+function Dog() {
+  this.name = "spot";
+  this.color = "blonde";
+  this.numLegs = 4;
+}
